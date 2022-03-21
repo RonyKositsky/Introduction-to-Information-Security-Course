489667fb9ee0974643bea689c132687e63636936
Rony Kositsky <ronykositsky@gmail.com>
2022-03-21 15:10:05 +0200

    //

---
+++ Ex5/q1.py
@@ -0,0 +1,40 @@
+import os
+import socket
+
+
+HOST = '127.0.0.1'
+PORT = 8000
+
+
+def get_payload() -> bytes:
+    """
+    This function returns the data to send over the socket to the server.
+
+    This data should cause the server to crash and generate a core dump. Make
+    sure to return a `bytes` object and not an `str` object.
+
+    WARNINGS:
+    0. Don't delete this function or change it's name/parameters - we are going
+       to test it directly in our tests, without running the main() function
+       below.
+
+    Returns:
+         The bytes of the payload.
+    """
+    # TODO: IMPLEMENT THIS FUNCTION
+    raise NotImplementedError()
+
+
+def main():
+    # WARNING: DON'T EDIT THIS FUNCTION!
+    payload = get_payload()
+    conn = socket.socket()
+    conn.connect((HOST, PORT))
+    try:
+        conn.sendall(payload)
+    finally:
+        conn.close()
+
+
+if __name__ == '__main__':
+    main()
